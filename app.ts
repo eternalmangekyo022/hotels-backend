911c4ad3c1d3af61b68d9a177e6bdd311e93c989
Tibor Fazekas <tibcso0322@gmail.com>
2024-11-18 23:42:44 +0100

    added type for req.user

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import e from "express";
+import e, { type NextFunction } from "express";
 import dotenv from "dotenv";
 import users from "./routes/users.routes";
 import jwt from "jsonwebtoken";
@@ -27,36 +27,40 @@ const reg = `^(?!.*(${excludeToken.join("|")})).*`;
 // ^(?!.*(${excludeList.join("|")})).*
 const createPathRegex = () => new RegExp(reg);
 const excludedTokenPath = createPathRegex();
-app.use(excludedTokenPath, (req: any, res: any, next) => {
-  const {
-    headers: { authorization },
-  } = req;
-  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
-  const token = authorization && authorization.split(" ")[1];
+app.use(
+  excludedTokenPath,
+  (req: Req<{ headers: { authorization: string } }>, res: any, next) => {
+    const {
+      headers: { authorization },
+    } = req;
+    if (!authorization)
+      return res.status(401).json({ message: "Unauthorized" });
+    const token = authorization && authorization.split(" ")[1];
 
-  try {
-    const verified = jwt.verify(token!, process.env.JWT_ACCESS_SECRET!);
-    req.user = verified as TokenVerified;
-    next();
-  } catch (error: Err | any) {
-    if (error.name === "TokenExpiredError") {
-      return res
-        .status(401)
-        .json({ message: "Token expired. Please refresh." });
+    try {
+      const verified = jwt.verify(token!, process.env.JWT_ACCESS_SECRET!);
+      req.user = verified as TokenVerified;
+      next();
+    } catch (error: Err | any) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Token expired. Please refresh." });
+      }
+      res.status(403).json({ message: "Invalid token" });
     }
-    res.status(403).json({ message: "Invalid token" });
   }
-});
+);
 
 users(use, app);
 hotels(use, app);
 
 api.use("/api/v1", app);
 
-api.use((error: Err | null, req: Req, res: Res, next: Next) => {
+api.use((error: Err | null, _req: Req, res: Res, _next: NextFunction) => {
   console.log("error", error);
   if (error?.message === "jwt expired")
-    res.status(401).json({ message: "Token e" });
+    res.status(401).json({ message: "Token expired" });
   else if (error) res.status(error.code).json({ message: error.message });
 });
 
