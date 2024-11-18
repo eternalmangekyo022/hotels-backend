6f21c7789306eb915fa5c94a7eb77cb724426755
Tibor Fazekas <tibcso0322@gmail.com>
2024-11-18 23:46:50 +0100

    refactor

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import users from "../controllers/users.controller";
+import * as users from "../controllers/users.controller";
 
 export default (use: UseFn, app: Express) => {
   const router = Router();
   router.post("/login", use(users.login));
   router.post("/register", use(users.register));
   router.post("/refresh", use(users.refresh));
+  //delete user
+  //update user
+  //get user
+  //patch user
 
   app.use("/", router);
 };
